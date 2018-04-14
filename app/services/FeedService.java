bfbe49eb92a495b7beaba1fe96584947cdc7c1a7
adithya1012 <adithyasn7@gmail.com>
2018-04-14 07:22:56 +0000

    Added Feedservice

--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,28 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query){
+         FeedResponse feedResponseObject=new FeedResponse();
+        try{
+            WSRequest feedRequest=WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise=feedRequest
+                    .setQueryParameter("q",query)
+                    .setQueryParameter("output","rss")
+                    .setQueryParameter("lang","en")
+                    .get();
+            Document response=responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            //response=compleat xml file
+            Node item=response.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponseObject.title   = item.getChildNodes().item(0).getNodeValue();
+            feedResponseObject.pubDate  = item.getChildNodes().item(3).getNodeValue();
+            feedResponseObject.description  = item.getChildNodes().item(4).getNodeValue();
+        }
+        catch(Exception e)
+        {
+
+        }
+        return feedResponseObject;
+    }
+
 
 }
